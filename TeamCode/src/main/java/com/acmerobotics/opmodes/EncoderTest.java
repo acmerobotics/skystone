68ca6117b79fd3e33439ee7d858d65b52fadf021
Jesus Betancourt <jbeta02@gmail.com>
2019-11-26 22:08:25 -0800

    encoder test code for arm is completely finished and ready for testing on robot. math to find a target position based on an angle is added to armEncoder. The test teleOP is set to make to main tests 1) test the setup and main encoder methods which include: init(), resetEncoder(), runTo() and more. 2) the encoder math to get the ticks for an angle which include: ticksPerInch(), arcLength(), and more. all of the methods on the armEncoder class have comment descriptions to easy understanding. the setup and main methods as well and the encoder math methods are seperated into 2 sections by comments to help in organization. in the teleOP there are 2 similar sectoins of code, the first part is to test the setup and main methods, the other is to test the encoder math methods. the encoder math portion of the teleOp (EncoderTest) is commented out so it doesn't interfere. When the testing of the first section is done comment out the runTo() method and the telementery and delete the comments taking the section teleOp secion out so you can test the encoder math methods, (delete the /* and */).

--- TeamCode/src/main/java/com/acmerobotics/opmodes/EncoderTest.java
+++ TeamCode/src/main/java/com/acmerobotics/opmodes/EncoderTest.java
@@ -21,14 +21,41 @@ public class EncoderTest extends LinearOpMode {
         waitForStart();
 
         while(!isStopRequested()){
-            arm.runTo(arm.position1, arm.thePower);
 
-            telemetry.addData("set position: ", arm.position1);
+            // description: test to make sure your setup and main encoder code is correct
+
+            // outcome: arm should move to the encoder position set (45) and it should hold that position
+            arm.runTo(arm.testPosition1, arm.thePower);
+
+            telemetry.addData("target position: ", arm.testPosition1);
             telemetry.addLine();
 
             telemetry.addData("current position: ", arm.armMotor.getCurrentPosition());
             telemetry.addData("power: ", arm.armMotor.getPower());
             telemetry.update();
+
+
+            // ^^^^can delete when testing is successful and uncomment the code below
+
+
+            /*
+
+            // TODO get the real ticks per rev value or code will NOT work
+
+            // description/ outcome: arm will raise to a 45 degree angle from the init position
+
+
+            arm.encoderRunTo(arm.testAngle);
+
+            telemetry.addData("angle: ", arm.testAngle);
+            telemetry.addData("target position: ", arm.testEncoderPosition);
+            telemetry.addLine();
+
+            telemetry.addData("current position: ", arm.armMotor.getCurrentPosition());
+            telemetry.addData("power: ", arm.armMotor.getPower());
+            telemetry.update();
+
+             */
         }
     }
 }
--- TeamCode/src/main/java/com/acmerobotics/robot/armEncoder.java
+++ TeamCode/src/main/java/com/acmerobotics/robot/armEncoder.java
@@ -8,18 +8,41 @@ public class armEncoder {
 
     public DcMotorEx armMotor;
 
-    private static double TICK_COUNT_PER_REVOLUTION = 280;
+    //TODO///////////// NEED REAL VALUES FOR VARIABLES, ALL OF THESE ARE FOR TESTING ONLY //////////
 
-    public int position1 = 0;
-    public int position2 = 0;
+    private static double TICKS_PER_REV = 280;
+
+    private double ARM_MOTOR_DIAMETER = 1;
+
+    private int gearRatio = 2;
+
+    public double testAngle = 10;
+
+    public int testEncoderPosition = 0;
+
+    //^^^^^^^^^^^^^^^^^^^^used in encoder math^^^^^^^^^^^//
+
+    public int testPosition1 = 45;
+
+    //^^^^^^^^^^^^^^^used in encoder test^^^^^^^^^^^^^^^//
 
     public double thePower = 0.85;
+
+    //^^^^^^^^^^^^general variables^^^^^^^^^^^^^^^^^^^^//
+
 
     public void armEncoder(){
     }
 
+    ////////////////////////////// encoder setup and main methods //////////////////////////////////
+
     public void init(HardwareMap hardwareMap){
+        // motor added to hardware map, arm floats to init position and
+        // is set to 0 power and is held there by RUN_USING_ENCODER
+
         armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
+
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         armMotor.setPower(0);
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -27,16 +50,24 @@ public class armEncoder {
 
 
     public void resetEncoder(){
+        // motor's current encoder position is set as the zero position
+
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
 
     public void leaveReset(){
+        // motor mode is set to RUN_USING_ENCODER to get motor out of STOP_AND_RESET mode
+        // motor will just continue to hold a 0 power
+
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
 
     public void runTo(int position, double power) {
+        // target position is set and the motor is set to run to that position and a set speed/ power
+        // target position is held with pid
+
         armMotor.setTargetPosition(position);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setPower(power);
@@ -44,9 +75,76 @@ public class armEncoder {
 
 
     public void stopMotors(){
+        // motor is stopped and its power is set to 0 so it will just fall to its init position and
+        // hold that position
         armMotor.setPower(0);
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-}
 
+    ///////////////////////////^ end of encoder setup and main methods ^//////////////////////////////
+
+
+    ///////////////////////////// math to get ticks to move at x angle /////////////////////////////
+
+    public double ticksPerInch(){
+        // number of ticks per inch based on the arm gear circumference
+
+        return TICKS_PER_REV/ Math.PI * ARM_MOTOR_DIAMETER;
+        // (to test if this is really ticks per inch: take the number that results from the equation above (x) and
+        // multiply it by the circumference, this number will tell you the total number of ticks around the circumference.)
+    }
+
+
+    public double arcLength(double angle ){
+        // get the length of the arc, in inches, of the arm motor gear based on an angle
+
+        return (angle/ 360) * (Math.PI * ARM_MOTOR_DIAMETER);
+        //     ^portion of circumference          ^circumference
+    }
+
+
+    public int arcInchesToTicks(double angle){
+        // get ticks of arc
+
+        double ticksPerInch = ticksPerInch();
+
+        double arcLength = arcLength(angle);
+
+        int ticksForArc = (int) ticksPerInch * (int) arcLength;
+
+        return ticksForArc; // ticks to get to the arc length, arc is based on given angle. (result is for motor gear)
+    }
+
+
+    public int ToArmGearTicks(double angle){
+        // convert motor gear ticks to the ticks needed to get the same result on the arm gear, this is done
+        // by multipling the motor gear ticks by the 2 because the arm to motor gear ratio is 2:1
+
+        int motorGearTicks = arcInchesToTicks(angle);
+
+        int armGearTicks = motorGearTicks * gearRatio;
+
+        return armGearTicks;
+    }
+
+
+    public int setEncoderTicks(double angle){
+        //final step of encoder math, by now the ticks of the arc have been found for the motor gear
+        // and this has been converted to find the ticks needed for the arm gear which is 2 times
+        // more than the motor gear arc tick count.
+
+        return ToArmGearTicks(angle);
+    }
+
+
+    public void encoderRunTo(double angle){
+        int position = setEncoderTicks(angle);
+
+        testEncoderPosition = position;/////////////////////////////////// used for a telemetry test
+
+        runTo(position, thePower);
+    }
+
+    //////////////////////////////////////^ end of encoder math ^/////////////////////////////////////
+}
